4385662d757af2708f72203fd4b27817bab1fb43
pionie <pioniewinski@gmail.com>
2024-12-08 23:45:48 +0100

    Update version to v1.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pypaq.lipytools.files import get_requirements
 
 setup(
     name=               'ompr',
-    version=            'v1.3.3',
+    version=            'v1.3.4',
     url=                'https://github.com/piteren/ompr.git',
     author=             'Piotr Niewinski',
     author_email=       'pioniewinski@gmail.com',
