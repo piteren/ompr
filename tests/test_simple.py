675cba042adedbc94af3698e4402bf357413c456
pionie <pioniewinski@gmail.com>
2026-02-16 19:22:04 +0100

    tests in pytests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,26 +1,23 @@
 import random
 import time
-import unittest
 
 from ompr.simple import simple_process
 
 
-class Test_simple(unittest.TestCase):
+def test_simple_process():
 
-    def test_simple_process(self):
+    def func(a:float, b:float) -> float:
+        time.sleep(1)
+        return a*b
 
-        def func(a:float, b:float) -> float:
-            time.sleep(1)
-            return a*b
+    num_tasks = 20
 
-        num_tasks = 20
+    tasks = [{'a':random.random(), 'b':random.random()} for _ in range(num_tasks)]
 
-        tasks = [{'a':random.random(), 'b':random.random()} for _ in range(num_tasks)]
-
-        res = simple_process(
-            tasks=          tasks,
-            function=       func,
-            loglevel=       10,
-            num_workers=    4)
-        print(len(res))
-        self.assertTrue(len(res)==num_tasks)
+    res = simple_process(
+        tasks=          tasks,
+        function=       func,
+        loglevel=       10,
+        num_workers=    4)
+    print(len(res))
+    assert len(res) == num_tasks
